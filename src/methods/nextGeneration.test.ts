521bd371503efeba9e5631900a9190d628acb3aa
13Coconut69 <ost1nus@yandex.ru>
2021-11-04 02:08:22 +0300

    Add: added some tests

--- src/methods/nextGeneration.test.ts
+++ src/methods/nextGeneration.test.ts
@@ -1,7 +1,8 @@
 import { CellularAutomaton } from "..";
+import { neighborhoodTypes } from "../enums/neighborhoodEnum";
 
 describe('nextGeneration() works correctly', () => {
-    test('rule B3/S23', () => {
+    test('[Moore] rule B3/S23', () => {
         const ca = new CellularAutomaton(3, 23, 5, 3);
         ca.createCell(0, 0);
         expect(ca.nextGeneration()).toStrictEqual([[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]);
@@ -11,14 +12,14 @@ describe('nextGeneration() works correctly', () => {
         ca.createCell(1, 0);
         expect(ca.nextGeneration()).toStrictEqual([[1, 1, 0], [1, 1, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]);
     });
-    test('rule B3/S123456780', () => {
+    test('[Moore] rule B3/S123456780', () => {
         const ca = new CellularAutomaton(3, 123456780, 5, 3);
         ca.createCell(0, 2);
         ca.createCell(1, 2);
         ca.createCell(2, 2);
         expect(ca.nextGeneration()).toStrictEqual([[0, 0, 0], [0, 1, 0], [1, 1, 1], [0, 1, 0], [0, 0, 0]]);
     });
-    test('rule B5678/S45678', () => {
+    test('[Moore] rule B5678/S45678', () => {
         const ca = new CellularAutomaton(5678, 45678, 5, 3);
         ca.createCell(0, 0);
         ca.createCell(1, 0);
@@ -26,5 +27,23 @@ describe('nextGeneration() works correctly', () => {
         ca.createCell(0, 1);
         ca.createCell(2, 1);
         expect(ca.nextGeneration()).toStrictEqual([[0, 1, 0], [0, 1, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]);
+    });
+    test("[vonNeumann] rule B3/S23", () =>{
+        const ca = new CellularAutomaton(3, 23, 5, 3, {
+            neighborhoodType: neighborhoodTypes.vonNeumann
+        });
+        ca.createCell(0, 0);
+        ca.createCell(0, 1);
+        ca.createCell(1, 0);
+        expect(ca.nextGeneration()).toStrictEqual([[1, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]);
+    });
+    test("[vonNeumann] rule B3/S123456780", () => {
+        const ca = new CellularAutomaton(3, 123456780, 5, 3, {
+            neighborhoodType: neighborhoodTypes.vonNeumann
+        });
+        ca.createCell(0, 2);
+        ca.createCell(1, 2);
+        ca.createCell(2, 2);
+        expect(ca.nextGeneration()).toStrictEqual([[0, 0, 0], [0, 0, 0], [1, 1, 1], [0, 0, 0], [0, 0, 0]]);
     });
 });
